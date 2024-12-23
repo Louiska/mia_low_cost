568a6f0a2fae3e875265a684f6b246d6ad016937
Louis Kerner <louiskerner@protonmail.com>
2024-12-23 01:03:21 +0100

    add logging for training configs

--- train_shadow_model.py
+++ train_shadow_model.py
@@ -4,22 +4,23 @@ from torch.utils.data import DataLoader
 from torch.nn import CrossEntropyLoss, Softmax
 from torch import optim
 from dataset_helper import create_subset
-
-def train_shadow_model(model, trainset, valset, save_path = "", num_epochs = 30):
+import csv
+def train_shadow_model(model, trainset, valset, save_path = "", num_epochs = 20, bs = 64, lr = 0.004):
     print("Training model for: " + save_path)
-    trainloader = DataLoader(trainset, batch_size = 64, num_workers = 16)
-    valloader = DataLoader(valset, batch_size = 64, num_workers = 16)
+    trainloader = DataLoader(trainset, batch_size = bs, num_workers = 16)
+    valloader = DataLoader(valset, batch_size = bs, num_workers = 16)
     dataloader = {"train": trainloader, "val": valloader}
     criterion = CrossEntropyLoss()
     #softmax = Softmax(dim=1)
     optimizer = optim.AdamW(
         model.parameters(),
-        lr = 0.004, 
+        lr = lr, 
     )
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(
         optimizer, patience = 3, factor = 0.4, verbose=True,
     )
-    hs_epoch_metric = 1000
+    best_epoch_metric = 1000
+    best_epoch = 0
     for epoch in range(num_epochs):
         print(f"Run epoch: {epoch}/{num_epochs}")
         for phase in ["train", "val"]:
@@ -29,10 +30,13 @@ def train_shadow_model(model, trainset, valset, save_path = "", num_epochs = 30)
                         criterion,
                         optimizer, 
                         epoch)
+            if phase == "train":
+                train_epoch_metric = epoch_metric
             if phase == "val":
                 scheduler.step(epoch_metric)
-                if epoch_metric<hs_epoch_metric:
-                    hs_epoch_metric = epoch_metric
+                if epoch_metric<best_epoch_metric:
+                    best_epoch_metric = epoch_metric
+                    best_epoch = epoch
                     counter_epoch_metric = 0
                     print(f"New best metric")
                     #torch.save(model.state_dict(), save_path + "_tmp.pt")
@@ -40,12 +44,25 @@ def train_shadow_model(model, trainset, valset, save_path = "", num_epochs = 30)
                 else:
                     counter_epoch_metric+=1
                     print(f"No improvement for {counter_epoch_metric} epochs at lr {scheduler.get_last_lr()}")
-
-                #if epoch+1 % 6 == 0:
-                #    torch.save(model.state_dict(), save_path + "_tmp_" +str(epoch) + ".pt")
                     
     torch.save(model.state_dict(), save_path + ".pt")
     print("Done, saved at " + save_path)
+    data = {"name": save_path,
+            "dataset_size":0.5, 
+            "num_epochs": num_epochs,
+            "lr": lr,
+            "bs": bs,
+            "last_train_metric": train_epoch_metric,
+            "last val_metric": epoch_metric,
+            "best_epoch_metric": best_epoch_metric,
+            "best_epoch": best_epoch,
+            "TPR@FPR=0.05":0,
+            "AUC":0.5}
+    with open("train_log.csv", "w") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data[0].keys())
+        writer.writeheader()
+        writer.writerows(data)
+    
 
 def run_epoch(model, phase, dataloader, criterion, optimizer, epoch):
     total_correct, total_loss = 0,0
