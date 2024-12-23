a77057b9b616125841e3be9cf964d81e34baa724
Louis Kerner <louiskerner@protonmail.com>
2024-12-23 13:21:21 +0100

    refactoring

--- model.py
+++ model.py
@@ -3,11 +3,7 @@ import torch
 
 # load the data from a file
 def get_model(name=""):
-    if name != "":
-        pretrained = False
-    else:
-        pretrained = True
-    model = resnet18(pretrained=pretrained)
+    model = resnet18(pretrained=True)
     model.fc = torch.nn.Linear(512, 44)
     if name != "":
         model.load_state_dict(torch.load(f"out/models/{name}.pt", map_location = "cpu"))
