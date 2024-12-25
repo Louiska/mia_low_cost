4dcb1240a8da3b20c9278c82c790a9440344d92a
Louis Kerner <louiskerner@protonmail.com>
2024-12-25 22:46:04 +0100

    train larger shadow model (resnet50)

--- main.py
+++ main.py
@@ -7,27 +7,32 @@ import random
 from torchvision.transforms import v2
 from mia import mia
 from dataset_helper import split_dataset, get_dataset
+from dataset import MembershipDataset
 
 TRAINSET_PATH = "out/data/01/pub.pt"
 TARGETSET_PATH = "out/data/01/priv_out.pt"
-SHADOW_MODEL_NAME = "model_v13"
+SHADOW_MODEL_NAME = "model_v15"
+TARGET_ARCHITECTURE = "resnet18"
+TARGET_MODEL_NAME = "target"
 
 
 def main(
     trainset_path: str,
     targetset_path: str,
-    shadow_models_name: str,
+    shadow_models_save_name: str,
     num_shadow_models: int,
-    train_models: bool = False,
+    model_architecture: str = "resnet18",
+    train: bool = False
 ):
     """Trains shadow models and forwards them to the MIA
 
     Args:
         trainset_path (str): Path to training set
         targetset_path (str): Path to set of to classify data
-        shadow_models_name (str): Name for saving/loading shadow models
+        shadow_models_save_name (str): Name for saving/loading shadow models.
         num_shadow_models (int): Aka. k, how many reference models should be trained
-        train_models (bool, optional): If true, trains shadow models, otherwise loads existing models. Defaults to False.
+        model_architecture (str, optional): The architecture of the model. Either resnet18 or resnet50. Defaults to resnet18
+        train (bool, optional): If true, trains new shadow models. Defaults to False
     """
     random.seed(0)
     train_transforms, target_transforms = get_transforms()
@@ -35,19 +40,19 @@ def main(
         trainset_path, targetset_path, train_transforms, target_transforms
     )
 
-    if train_models:
-        shadow_models_save_path = f"out/models/{shadow_models_name}"
+    if train:
+        shadow_models_save_path = f"out/models/{shadow_models_save_name}"
         if num_shadow_models > 1:
             shadow_models_sets = split_dataset(
                 trainset, replacement=True, k=num_shadow_models, trainset_size=1
             )  # Create trainset of trainset_size that contains the same ratio of member/non members
         else:
-            shadow_models_sets = trainset
-        for i, sub_trainset in enumerate(shadow_models_sets):
+            shadow_models_sets = [trainset]
+        for i, shadow_model_set in enumerate(shadow_models_sets):
             print(f"Training shadow model: {i}/{num_shadow_models}")
             train_shadow_model(
-                get_model(""),
-                sub_trainset,
+                get_model(model_architecture),
+                shadow_model_set,
                 targetset,
                 f"{shadow_models_save_path}_{i}",
                 num_epochs=20,
@@ -58,8 +63,12 @@ def main(
     # Load shadow and target models
     shadow_models = []
     for i in range(num_shadow_models):
-        shadow_models.append(get_model(f"{shadow_models_name}_{i}_best").eval())
-    target_model = get_model("target").eval()  # lol so many models in RAM
+        shadow_models.append(
+            get_model(model_architecture, f"{shadow_models_save_name}_{i}_best").eval()
+        )
+    target_model = get_model(
+        TARGET_ARCHITECTURE, TARGET_MODEL_NAME
+    ).eval()  # lol so many models in RAM
 
     # Prepare Z set
     non_member_indices = [i for i, x in enumerate(trainset.membership) if x == 0]
@@ -73,9 +82,9 @@ def main(
         shadow_models,
         target_model,
         Z_set=Z_set,
-        save_name=shadow_models_name,
+        save_name=shadow_models_save_name,
         Z=2000,
-        alpha=0.3,
+        alpha=0.8,
     )
 
 
@@ -92,7 +101,8 @@ def get_transforms():
         ]
     )
     target_transforms = v2.Compose(
-        [v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+        [v2.Resize((224,224)),
+        v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
     )
     return train_transforms, target_transforms
 
@@ -101,6 +111,7 @@ main(
     TRAINSET_PATH,
     TARGETSET_PATH,
     SHADOW_MODEL_NAME,
-    num_shadow_models=2,
-    train_models=False,
+    num_shadow_models=1,
+    model_architecture="resnet50",
+    train=True
 )
--- model.py
+++ model.py
@@ -1,9 +1,9 @@
-from torchvision.models import resnet18
+from torchvision.models import resnet18, resnet50
 import torch
 from torch.nn import Module
 
 # load the data from a file
-def get_model(name: str = "") -> Module:
+def get_model(architecture: str = "resnet18", pretrained_model: str = None) -> Module:
     """
     Args:
         name (str, optional): File name of model that's supposed to be loaded. If "", 
@@ -12,8 +12,16 @@ def get_model(name: str = "") -> Module:
     Returns:
         (Module): The loaded model
     """
-    model = resnet18(pretrained=True)
-    model.fc = torch.nn.Linear(512, 44)
-    if name != "":
-        model.load_state_dict(torch.load(f"out/models/{name}.pt", map_location="cpu"))
+    if architecture == "resnet50":
+        model = resnet50(pretrained=True)
+        model.fc = torch.nn.Linear(2048, 44)
+    elif architecture == "resnet18":
+        model = resnet18(pretrained=True)
+        model.fc = torch.nn.Linear(512, 44)
+    else:
+        print("Architecture not supported, try resnet18 or resnet50")
+        exit(1)
+    if pretrained_model != None:
+        model.load_state_dict(torch.load(f"out/models/{pretrained_model}.pt", map_location="cpu"))
+    
     return model
