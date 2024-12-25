9e3231f560cf6226f0004156549f3fcd8b8722df
Louis Kerner <louiskerner@protonmail.com>
2024-12-25 20:23:58 +0100

    remove unused analyse_dataset

--- dataset_helper.py
+++ dataset_helper.py
@@ -1,15 +1,6 @@
 from torch.utils.data import Subset, Dataset
 import random
 
-
-def analyse_dataset(dataset: Dataset):
-    labels = {i: dataset.labels.count(i) for i in dataset.labels}
-    print(f"Length: {len(dataset)}")
-    print("Label: n")
-    print(labels)
-    return dataset.ids, labels
-
-
 def create_subset(trainset: Dataset, k: int):
     selected_ids = random.sample(range(len(trainset)), k)
     subset = Subset(trainset, selected_ids)
