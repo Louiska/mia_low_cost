728e59d88ba13e7b8ffa644ecb7e8d4d5eb17f41
Louis Kerner <louiskerner@protonmail.com>
2024-12-25 20:23:34 +0100

    remove normalization before plotting image

--- visualize.py
+++ visualize.py
@@ -1,6 +1,13 @@
+
+import torch
 import matplotlib.pyplot as plt
 
+means = torch.tensor([0.485, 0.456, 0.406])
+stds = torch.tensor([0.229, 0.224, 0.225])
 
-def show_img(img):
-    plt.imshow(img.permute(1, 2, 0))
+def show_img(img:torch.tensor):
+    img = img.permute(1,2,0)
+    img = img * stds + means
+    print(img)
+    plt.imshow(img)
     plt.show()
